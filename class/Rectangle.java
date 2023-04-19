20aa3265243bcb954b7619d72877d3497dedac78
Adrian400811 <adrianleeho@gmail.com>
2023-04-19 09:25:29 -0400

    Rearrange files

---
+++ class/Rectangle.java
@@ -0,0 +1,13 @@
+Public class Rectangle{
+	private int width;
+	private int length;
+
+	public Rectangle(int theWidth, int theLength) {
+		width = theWidth;
+		length = theLength;
+	}
+
+	public getArea() {
+		return width * length;
+	}
+}
