9f353b5eefd09baa0b4a4206ebb0a428b2b1f863
AdrianL40 <adrianleeho@gmail.com>
2023-04-25 09:04:55 -0400

    Update Dog.java

--- Dog.java
+++ Dog.java
@@ -1,13 +1,15 @@
 public class Dog{
 	private String name;
 	private int age;
+	private boolean isDead;
 
-	public Dog(String theName; int theAge){
+	public Dog(String theName; int theAge, boolean theDead){
 		name = theName;
 		age = theAge;
+		isDead = theDead
 	}
 
 	public toString(){
-		System.out.println(name+" is "+age+" years old.");
+		System.out.println(name+" is "+age+" years old and Death="+isDead);
 	}
 }
